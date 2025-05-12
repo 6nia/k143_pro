e911cab744323a0ae3f072f1dd1b4d2f48ffecab
6nia <140215435+6nia@users.noreply.github.com>
2025-05-13 00:01:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7949227263:AAGqSx4PiF2loVT8GGTPSU3OxaIRaXI8ahA")
+API_ID = int(os.environ.get("API_ID", "18489236"))
+API_HASH = os.environ.get("API_HASH", "f69613814cdb130946c5f20acbef3d56")
 SPOILER_MODE = bool(os.environ.get("SPOILER_MODE", True))
